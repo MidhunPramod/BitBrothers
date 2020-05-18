44d67ac116589694d99600255614f14c0711f286
midhun <midhunstab@gmail.com>
2020-05-18 10:33:08 +0530

    changed mongouri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOURI, {
+    await mongoose.connect(`${process.env.MONGOURI}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
